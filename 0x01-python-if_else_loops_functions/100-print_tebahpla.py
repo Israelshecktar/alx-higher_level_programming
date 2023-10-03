35a32421b628482324d304a1e72594f556ab4237
Israelshecktar <iamshecktar1996@gmail.com>
2023-10-03 12:20:01 +0100

    script prints alphabets in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{}".format(chr(i - 32) if i % 2 == 0 else chr(i)), end="")
+output = ""
+upper = False
+
+for char in range(ord('z'), ord('a') - 1, -1):
+    if upper:
+        output += chr(char).upper()
+    else:
+        output += chr(char).lower()
+    upper = not upper
+
+print("{}".format(output), end="")
