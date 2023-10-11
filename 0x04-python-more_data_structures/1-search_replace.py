b2b555d3e894ea006cf29768d2bdceba9eb4ac0c
Israelshecktar <iamshecktar1996@gmail.com>
2023-10-11 14:34:12 +0100

    function that replaces all occurrences of an element by another in a new list.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """Function that replaces all occurrences of an element by another"""
+    return list(map(lambda x: x if x != search else replace, my_list))
