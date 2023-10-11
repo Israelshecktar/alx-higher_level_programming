5b71dbc6519146d6248c0b915351e42862a1bff2
Israelshecktar <iamshecktar1996@gmail.com>
2023-10-11 15:31:30 +0100

    a function that deletes keys with a specific value in a dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda n: n ** 2, row)), matrix))
+def complex_delete(a_dictionary, value):
+#Function that deletes keys with a specific value in a dictionary
+    to_del = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            to_del.append(key)
+    for key in to_del:
+        del a_dictionary[key]
+    return a_dictionary
