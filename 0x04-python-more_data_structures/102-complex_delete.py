31677b50c8c97bd93d08ea3fc5aac67d44f52c1b
Israelshecktar <iamshecktar1996@gmail.com>
2023-10-11 15:46:55 +0100

    functions that computes data on Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-#Function that deletes keys with a specific value in a dictionary
     to_del = []
     for key, val in a_dictionary.items():
         if val == value:
