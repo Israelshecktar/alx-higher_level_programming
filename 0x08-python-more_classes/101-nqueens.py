0485dea9db4005340c37b648d7f2ea6a30f93589
Israelshecktar <iamshecktar1996@gmail.com>
2023-10-30 14:00:58 +0100

    nqueens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+import sys
+
+def print_result(board):
+    res = []
+    for i in range(len(board)):
+        res.append([i, board[i]])
+    print(res)
+
+
+def is_valid(board, current_queen):
+    for queen in range(current_queen):
+        if (board[queen] == board[current_queen] or
+                board[queen] - queen == board[current_queen] - current_queen or
+                board[queen] + queen == board[current_queen] + current_queen):
+            return False
+    return True
+
+
+def place_queens(board, n, current_queen):
+    if current_queen == n:
+        print_result(board)
+        return
+
+    for column in range(n):
+        board[current_queen] = column
+        if is_valid(board, current_queen):
+            place_queens(board, n, current_queen + 1)
+
+
+def check_args():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if not (sys.argv[1]).isdigit():
+        print("N must be a number")
+        exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+
+if __name__ == "__main__":
+    check_args()
+    n = int(sys.argv[1])
+    board = [-1 for _ in range(n)]
+    place_queens(board, n, 0)
