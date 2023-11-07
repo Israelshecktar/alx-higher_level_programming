49d2b230dcd3ce96f1ccc9b9c409cadd8e479a9f
Israelshecktar <iamshecktar1996@gmail.com>
2023-11-07 17:40:24 +0100

    a script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,16 +5,20 @@ line and computes metrics.
 """
 import sys
 
+
 file_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
+
 def print_stats():
     """Prints the file size and the status code counts."""
+
     print("File size: {}".format(file_size))
     for code, count in sorted(status_codes.items()):
         if count > 0:
             print("{}: {}".format(code, count))
+
 
 try:
     for line in sys.stdin:
