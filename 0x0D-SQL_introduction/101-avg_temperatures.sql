a4e580f18a42621dc314bdc1d39458b80f6cbbfc
Israelshecktar <iamshecktar1996@gmail.com>
2023-12-12 18:21:22 +0100

    Import in hbtn_0c_0 database this table dump

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
--- Display the average temperature by city
-SELECT city, AVG(temp) AS avg_temp FROM temperature GROUP BY city ORDER BY avg_temp DESC;
+-- This command displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
