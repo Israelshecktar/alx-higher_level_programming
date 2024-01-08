4505a7418503d8fb01c3f1dff79e752e1167962c
Israelshecktar <iamshecktar1996@gmail.com>
2024-01-08 13:16:15 +0100

    fixed task2 errs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // A script that prints a message depending on the number of arguments
 
-const args = process.argv.length - 2;
+let args = process.argv.length - 2;
 if (args === 0) {
   console.log('No argument');
 } else if (args === 1) {
