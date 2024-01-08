1605d58a63517e64eacda9f9d7fdd23d3ef4faca
Israelshecktar <iamshecktar1996@gmail.com>
2024-01-08 22:17:29 +0100

    Update 12-object.js

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-// This script replaces the value 12 with 89:
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-/*
-  YOUR CODE HERE
-  */
 myObject.value = 89;
 console.log(myObject);
