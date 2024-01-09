3879ac7a6a4aeb6f4ebd253076321442413674bc
Israelshecktar <iamshecktar1996@gmail.com>
2024-01-09 17:22:23 +0100

    fixed bugs

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,7 +5,7 @@ class Rectangle {
   constructor (w, h) {
     if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
       this.width = w;
-      this.heigth = h;
+      this.height = h;
     }
   }
 }
