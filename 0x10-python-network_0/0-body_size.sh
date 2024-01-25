9e4dd47a6e54fadf0029d67f70e8329654eb343d
Israelshecktar <iamshecktar1996@gmail.com>
2024-01-25 17:55:36 +0100

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# This script takes a URL as input and displays the size of the response body in bytes
+# This script takes a URL as input and displays
 
-url=$1
-
-size=$(curl -si $url | awk '/Content-Length/{print $2}')
-echo $size
+url=$1 size=$(curl -si $url | awk '/Content-Length/{print $2}') echo $size
